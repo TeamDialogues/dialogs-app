98169c090e34b839986112017c9568f7d375e42f
py93 <47155755+py93@users.noreply.github.com>
2021-05-01 13:12:44 +0530

    feat: create,save,live chat

--- dialogs-app/src/App.js
+++ dialogs-app/src/App.js
@@ -1,7 +1,125 @@
+import React, { useRef, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import "firebase/firestore";
+import "firebase/auth";
+import firebase from "./config/firebaseConfig";
 import "./App.css";
 
+const firestore = firebase.firestore();
+
+function ChatRoom({ chatRoom }) {
+  const dummy = useRef();
+  const messagesRef = firestore.collection("messages");
+
+  const query = messagesRef
+    .where("chatRoomId", "==", chatRoom.id)
+    .orderBy("createdAt");
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  async function sendMessage(e) {
+    e.preventDefault();
+
+    await messagesRef.add({
+      userId: "123",
+      userName: "Pooja",
+      text: formValue,
+      chatRoomId: chatRoom.id,
+      createdAt: new Date().toISOString(),
+    });
+
+    setFormValue("");
+
+    dummy.current.scrollIntoView({ behaviour: "smooth" });
+  }
+
+  async function discardChat() {
+    const chatRoomsRef = firestore.collection("chatrooms").doc(chatRoom.id);
+
+    await chatRoomsRef.update({ currentStatus: "unsaved" });
+  }
+
+  async function saveChat() {
+    const chatRoomsRef = firestore.collection("chatrooms").doc(chatRoom.id);
+
+    await chatRoomsRef.update({ currentStatus: "saved" });
+  }
+
+  return (
+    <>
+      <h4>{chatRoom.title}</h4>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+
+      {chatRoom.currentStatus === "ongoing" ? (
+        <div>
+          <form onSubmit={sendMessage}>
+            <input
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
+            />
+            <button type="submit">Send</button>
+          </form>
+          <button onClick={saveChat}>save chat</button>
+          <button onClick={discardChat}>discard chat</button>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+function ChatMessage({ message }) {
+  return (
+    <div>
+      <img src={message.photoURL} />
+      <p>{message.text}</p>
+    </div>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  const chatRoomsRef = firestore.collection("chatrooms");
+  const query = chatRoomsRef.orderBy("createdAt");
+
+  const [chatRooms] = useCollectionData(query, { idField: "id" });
+
+  async function createRoom() {
+    const newRoom = {
+      userId: "123",
+      userName: "Pooja",
+      title: "WFH vs Office",
+      agenda: "To discuss about WFH vs office. Which one is better",
+      currentStatus: "ongoing", //has to be made ENUM
+      createdAt: new Date().toISOString(),
+    };
+
+    const response = await chatRoomsRef.add(newRoom);
+    console.log(response);
+  }
+  return (
+    <div className="App">
+      <button onClick={createRoom}>Create Room</button>
+      <br />
+      <heading>On going chats</heading>
+      {chatRooms &&
+        chatRooms
+          .filter((room) => room.currentStatus === "ongoing")
+          .map((room) => <ChatRoom key={room.id} chatRoom={room} />)}
+      <hr />
+      <heading>Saved Chats</heading>
+      {chatRooms &&
+        chatRooms
+          .filter((room) => room.currentStatus === "saved")
+          .map((room) => <ChatRoom key={room.id} chatRoom={room} />)}
+    </div>
+  );
 }
 
 export default App;
