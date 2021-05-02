e5f4ba94eeff85734ef1ef1063a4be327427d834
py93 <47155755+py93@users.noreply.github.com>
2021-05-02 12:34:44 +0530

    refact: resolve merge conflict

--- dialogs-app/src/components/ChatRoom/ChatRoom.jsx
+++ dialogs-app/src/components/ChatRoom/ChatRoom.jsx
@@ -1,26 +1,89 @@
-import { useState } from "react";
-import { currentUser, emojis, chat, users, host } from "../../temp-database";
+import { useEffect, useState } from "react";
+import { currentUser, emojis, chat } from "../../temp-database";
+
 import "./chat-room.css";
+import { UserListItem } from "./UsersListItem";
 
 export function ChatRoom() {
   const [showEmojiContainer, setEmojiContainerVisibility] = useState(false);
   const [textMessage, setTextMessage] = useState("");
+  const [showHamburger, setHamburger] = useState(false);
+
+  const hostOfTheChat = chat.users.find(
+    ({ permission }) => permission === "ADMIN"
+  );
+
+  const currentUserInfoFromChat = chat.users.find(
+    ({ id }) => id === currentUser.id
+  );
+
+  const isCurrentUserAdmin = hostOfTheChat.id === currentUserInfoFromChat.id;
+
+  const activeUsers = chat.users.filter(
+    ({ id, permission }) => id !== currentUser.id && permission === "WRITE"
+  );
+  const guestUsers = chat.users.filter(
+    ({ id, permission }) => id !== currentUser.id && permission === "READ"
+  );
+  const requestedUsers = chat.users.filter(
+    ({ id, permission }) => id !== currentUser.id && permission === "REQUEST"
+  );
+
+  const sortedUsers = isCurrentUserAdmin
+    ? [hostOfTheChat, ...requestedUsers, ...activeUsers, ...guestUsers]
+    : [
+        hostOfTheChat,
+        currentUserInfoFromChat,
+        ...requestedUsers,
+        ...activeUsers,
+        ...guestUsers,
+      ];
+
+  useEffect(() => {
+    //make user's permission in chat model- read
+    return () => {
+      //remove user from users array in chat model
+    };
+  });
   return (
     <div className="grid-30-70-layout">
-      <div className="grid-item-1 grid-item">
-        <ul>
-          {users.map((user) => {
-            return <li key={user.id}>{user.name}</li>;
-          })}
+      <div className={`grid-item-1 grid-item ${showHamburger && "active"}`}>
+        <button
+          className="hamburger-cross"
+          onClick={() => {
+            setHamburger((flag) => !flag);
+          }}
+        >
+          <i className="fas fa-times"></i>
+        </button>
+        <button className="btn btn-secondary-solid raise-request-btn">
+          <i className="fas fa-hand-paper margin-right-4px"></i>Raise Request To
+          Join Chat
+        </button>
+        <ul className="users-list">
+          {sortedUsers.map((user) => (
+            <UserListItem
+              key={user.id}
+              user={user}
+              isCurrentUserAdmin={isCurrentUserAdmin}
+            />
+          ))}
         </ul>
-
-        <button className="btn btn-secondary-solid">Create</button>
       </div>
+
       <div className="grid-item-2 grid-item">
         <div className="chat-details">
           <h1>{chat.title}</h1>
-          <p>{chat.agenda}</p>
+          <p className="text-color-secondary">{chat.agenda}</p>
         </div>
+        <button
+          className="btn btn-open-users-hamburger"
+          onClick={() => {
+            setHamburger(true);
+          }}
+        >
+          Users Joined
+        </button>
 
         <div className="divider-overlay">
           <span className="chat-date">{chat.createdAt.toDateString()}</span>
@@ -55,7 +118,7 @@ export function ChatRoom() {
                   {message.sender.name} {message.createdAt.getHours()}:
                   {message.createdAt.getMinutes()}
                 </div>
-                {currentUser.id === host.id && (
+                {currentUser.id === hostOfTheChat.id && (
                   <button className="star-on-message btn">
                     <i className="fas fa-star"></i>
                   </button>
@@ -65,7 +128,12 @@ export function ChatRoom() {
           );
         })}
 
+        <div id="last-div"></div>
         <div className="input-message-wrapper">
+          <a href="#last-div" className="link jump-to-present-button">
+            Jump to present <i className="fas fa-chevron-down"></i>
+          </a>
+
           {showEmojiContainer && (
             <div className="emoji-container">
               {emojis.map((emoji) => (
@@ -81,12 +149,12 @@ export function ChatRoom() {
               ))}
             </div>
           )}
-          <button
+          <div
             className="btn btn-icon"
             onClick={() => setEmojiContainerVisibility((flag) => !flag)}
           >
             <i className="far fa-grin-beam"></i>
-          </button>
+          </div>
           <input
             disabled={
               chat.users.find(
